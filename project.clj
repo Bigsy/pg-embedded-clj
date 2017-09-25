83f7206b44908c3c283da3c2f47b58d2efb2a755
Bill Hedworth <bill.hedworth@gmail.com>
2017-09-25 19:47:32 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bigsy/pg-embedded-clj "0.0.3"
+(defproject bigsy/pg-embedded-clj "0.0.4"
   :description "Embedded postgres for clojure"
   :url "https://github.com/Bigsy/pg-embedded-clj"
   :license {:name "Eclipse Public License"
